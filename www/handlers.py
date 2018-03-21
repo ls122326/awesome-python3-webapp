67003c66a2571fedcc1d8af70bd1880348d56a3e
lishen <305267243@qq.com>
2018-03-21 14:31:21 +0800

    简单web框架，没有页面相关

---
+++ www/handlers.py
@@ -0,0 +1,20 @@
+import re
+import time
+import json
+import logging
+import hashlib
+import base64
+import asyncio
+
+from coroweb import get, post
+
+from models import User, Comment, Blog, next_id
+
+
+@get('/')
+async def index(request):
+    users = await User.findAll()
+    return {
+        '__template__': 'test.html',
+        'users': users
+    }
